aeb59ed9645e227c719bd2b967a974cc775c30a3
Will Dougherty <doughertyw25@gmail.com>
2025-10-05 08:24:08 -0400

    problem done

--- problemset/05_sliding_window_max.py
+++ problemset/05_sliding_window_max.py
@@ -3,37 +3,33 @@ Problem: Sliding Window Maximum (Monotonic Deque)
 -------------------------------------------------
 Given an integer array `nums` and an integer `k`, return an array of the maximum values in every contiguous subarray of size `k`.
 
-Approach (Deque of indices, decreasing values):
-- Maintain deque with indices whose values are decreasing.
-- Pop from left when index is out of window; pop from right while current value >= last value.
-- The front is the max for the current window.
+Approach:
+If I did brute-force, the runtime would be O(n * k)
+I can reduce this...
 
-Time: O(n) | Space: O(k)
+
 """
 from collections import deque
 from typing import List
 
+def brute_force (nums: List[int], k: int) -> List[int]:
+    ra = []
+    if len(nums) < k or len(nums) == 0:
+        return []
+    for i in range(len(nums) - k+1):
+        cur = nums[i:i+k]
+        ra.append(max(cur))
+    return ra
+
 def max_sliding_window(nums: List[int], k: int) -> List[int]:
-    if k <= 0 or k > len(nums):
-        raise ValueError("k must be in 1..len(nums)")
-    dq = deque()  # stores indices
-    out = []
-    for i, x in enumerate(nums):
-        # remove out-of-window indices
-        if dq and dq[0] <= i - k:
-            dq.popleft()
-        # maintain decreasing deque
-        while dq and nums[dq[-1]] <= x:
-            dq.pop()
-        dq.append(i)
-        if i >= k - 1:
-            out.append(nums[dq[0]])
-    return out
+    return brute_force(nums, k)
 
 def _test():
     assert max_sliding_window([1,3,-1,-3,5,3,6,7], 3) == [3,3,5,5,6,7]
     assert max_sliding_window([1], 1) == [1]
+    assert max_sliding_window([], 0) == []
     assert max_sliding_window([9,8,7,6,5], 2) == [9,8,7,6]
+    assert max_sliding_window([9, 2, -1, -2, -1, 0, 4], 3) == [9, 2, -1, 0, 4]
 
 if __name__ == "__main__":
     _test()
